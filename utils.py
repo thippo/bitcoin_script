602e78823142cf29294533f46b79664b345e8f8f
thippo <thippo@163.com>
2016-08-07 18:44:12 +0800

    somefunctions

--- utils.py
+++ utils.py
@@ -2,3 +2,20 @@ import base58
 
 def WIF_to_compressed(privatekey_WIF):
     return base58.b58encode_check(base58.b58decode_check(privatekey_WIF)+b'\x01')
+
+def compressed_to_WIF(privatekey_compressed):
+    return base58.b58encode_check(base58.b58decode_check(privatekey_compressed)[:-1])
+
+def whether_privatekey(privatekey):
+    try:
+        private_key_decode = base58.b58decode_check(privatekey)
+        return True
+    except:
+        return False
+
+def whether_compressed(privatekey):
+    private_key_decode = base58.b58decode_check(privatekey)
+    if len(private_key_decode) ==33:
+        return False
+    elif len(private_key_decode) ==34:
+        return True
